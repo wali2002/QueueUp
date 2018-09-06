c11c26f729c2ecda8238264fb4ddca99b96a66bc
Sabir Rustaqi <sabir_2002@hotmail.com>
2018-09-05 21:30:02 -0400

    start of project

---
+++ app/src/main/java/com/company/sjd/queueup/MainActivity.java
@@ -0,0 +1,19 @@
+package com.company.sjd.queueup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); }
+
+
+
+}
